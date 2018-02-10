d6f41accdf7e17827fd04b410725e2ac392e3806
Chris <christopheramott@sky.com>
2018-02-10 17:00:02 +0000

    Basic DB setup

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://chris:password@ds229388.mlab.com:29388/hackathon';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
